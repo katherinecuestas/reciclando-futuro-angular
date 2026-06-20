45895d712af4124c6d70e178d2bcd744d4bab087
Katherine Cuestas <ka.cuestas@duocuc.cl>
2026-06-19 22:02:53 -0400

    feat: hero completado con imagen y difuminado

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent,NavbarComponent],
+  imports: [RouterOutlet, TopBarComponent,NavbarComponent,HeroComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
