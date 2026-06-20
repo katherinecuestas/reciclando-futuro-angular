023aa67eed688d5776a4119a9a2dbd672f29c3b9
Katherine Cuestas <ka.cuestas@duocuc.cl>
2026-06-19 22:41:07 -0400

    feat: seccion stats completada

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { StatsComponent } from './components/stats/stats.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent,NavbarComponent,HeroComponent],
+  imports: [RouterOutlet, TopBarComponent,NavbarComponent,HeroComponent,StatsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/stats/stats.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stats',
+  standalone: true,
+  templateUrl: './stats.component.html'
+})
+export class StatsComponent {}
