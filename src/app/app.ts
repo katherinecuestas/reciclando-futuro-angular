780519ffe58fab5c315c3f8ac564b8367f4325e0
Katherine Cuestas <ka.cuestas@duocuc.cl>
2026-06-20 00:09:22 -0400

    feat: seccion nosotros completada

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { StatsComponent } from './components/stats/stats.component';
 import { ProcesoComponent } from './components/proceso/proceso.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent,NavbarComponent,HeroComponent,StatsComponent,ProcesoComponent],
+  imports: [RouterOutlet, TopBarComponent,NavbarComponent,HeroComponent,StatsComponent,ProcesoComponent,NosotrosComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
